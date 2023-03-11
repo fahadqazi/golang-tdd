c57465661dc6404e4544703e9d5a1d74f6a1b6e6
Fahad Qazi <fahadqazi@gmail.com>
2023-03-11 06:25:57 +0000

    Refactor

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import "fmt"
 
 const englishHelloPrefix = "Hello,"
 const spanishHelloPrefix = "Hola,"
+const frenchHelloPrefix = "Bonjour,"
 
 func Hello(name string, language string) string {
 	if name == "" {
@@ -12,6 +13,10 @@ func Hello(name string, language string) string {
 
 	if language == "Spanish" {
 		return fmt.Sprintf("%s %s", spanishHelloPrefix, name)
+	}
+
+	if language == "French" {
+		return fmt.Sprintf("%s %s", frenchHelloPrefix, name)
 	}
 
 	return fmt.Sprintf("%s %s", englishHelloPrefix, name)
