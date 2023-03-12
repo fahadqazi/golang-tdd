6d9f10e81494f20b728f665d9642d62e1c81804f
Fahad Qazi <fahadqazi@gmail.com>
2023-03-12 05:29:11 +0000

    write tests

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -6,17 +6,28 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
 
 func ExampleSum() {
-	sum := Sum([5]int{1, 2, 3, 4, 5})
+	sum := Sum([]int{1, 2, 3, 4, 5})
 	fmt.Println(sum)
 	// Output: 15
 }
