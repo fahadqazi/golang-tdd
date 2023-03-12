bd0862634894307712fdd61f1837b97bb507d5b6
Fahad Qazi <fahadqazi@gmail.com>
2023-03-12 08:06:45 +0000

    add test & abstract method

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -47,3 +47,33 @@ func ExampleSumAll() {
 	fmt.Println(sum)
 	// Output: [3 7 11]
 }
+
+func TestSumTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4, 5})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
+
+func ExampleSumAllTails() {
+	sum := SumAllTails([]int{1, 2, 3}, []int{3, 4, 5})
+	fmt.Println(sum)
+	// Output: [5 9]
+}
